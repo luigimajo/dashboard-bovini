db48c6bd2c8f9ebfa7087b473dbcb1c796b5e5da
luigimajo <luigimajo@yahoo.it>
2026-02-28 14:45:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ m = folium.Map(location=[c_lat, c_lon], zoom_start=18, tiles=None)
 
 # SATELLITE GOOGLE FISSO
 folium.TileLayer(
-    tiles='https://mt1.google.com{x}&y={y}&z={z}',
+    tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
     attr='Google Satellite',
     name='Google Satellite',
     overlay=False,
